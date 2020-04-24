288ab5cbaa2d0442cad193e3e137a13661861a3c
Mengyue Yuan <33271377+yuanmengyue5566@users.noreply.github.com>
2020-04-24 17:54:11 +0800

    Add files via upload

---
+++ jpeg_compress.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include "jpeg_encoder.h"
+#include<iostream>
+using namespace std;
+
+//-------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+
+	JpegEncoder encoder;
+	encoder.readFromBMP("lena.bmp");
+	encoder.encodeToJPG("out1.jpg", 1);
+
+	return 0;
+}
+
